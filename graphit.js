824ea7df2dc504ab2fe3185e81cd09ceb7ff8678
Andrew R <andrewreds@gmail.com>
2010-05-01 20:41:35 +1000

    test

--- graphit.js
+++ graphit.js
@@ -1,15 +1,51 @@
 bar = {};
+line = {};
 
-window.onload = function () {
-	var data = [280,45,133,166,84,259,266,960,219,311];
+graphit = function (names,values,data1) {
 
-	bar = new RGraph.HBar('myCanvas', data);
-	bar.Set('chart.labels', ['Richard', 'Alex', 'Nick', 'Scott', 'Kjnell', 'Doug', 'Charles', 'Michelle', 'Mark', 'Alison']);
+	document.getElementById('overallUsage').width = document.getElementById('overallUsage').width;
+	document.getElementById('hourUsage').width = document.getElementById('hourUsage').width;
+	
+	
+	bar = new RGraph.HBar('overallUsage', values);
+	bar.Set('chart.labels', names);
 	bar.Set('chart.gutter', 45);
 	bar.Set('chart.background.barcolor1', 'rgba(255,255,255,1)');
 	bar.Set('chart.background.barcolor2', 'rgba(255,255,255,1)');
 	bar.Set('chart.background.grid', true);
-	bar.Set('chart.colors', ['rgba(255,0,0,1)']);
+	bar.Set('chart.colors', ['red', 'orange','yellow','green','blue','purple','black','cyan','Chocolate','Magenta']);
 		
 	bar.Draw();
+
+
+	line = new RGraph.Line("hourUsage", data1[0], data1[1], data1[2], data1[3], data1[4], data1[5], data1[6], data1[7], data1[8], data1[9]);
+	line.Set('chart.background.barcolor1', 'rgba(255,255,255,1)');
+	line.Set('chart.background.barcolor2', 'rgba(255,255,255,1)');
+	line.Set('chart.background.grid.color', 'rgba(238,238,238,1)');
+	line.Set('chart.colors', ['red', 'orange','yellow','green','blue','purple','black','cyan','Chocolate','Magenta']);
+	line.Set('chart.linewidth', 2);
+	line.Set('chart.filled', false);
+	line.Set('chart.hmargin', 5);
+	line.Set('chart.labels', []);
+	line.Set('chart.gutter', 40);
+	line.Draw();
+};
+
+window.onload = function () {
+	data1 = [];
+	
+	for (i=0;i<10;i++) {
+		data1[i] = [];
+		curr=i+10;
+		for (a=0;a<100;a++) {
+			curr = Math.random()*2-1+curr;
+			if (curr < 0) {curr = 0;}
+			data1[i][a] = curr;
+		}
+	}
+	
+	graphit (["facebook.com","google.com.au","mail.google.com"],[[40,25,15]],data1);
+	
+	window.setTimeout ('graphit (["facebook.com","google.com.au","mail.google.com"],[[30,5,66]],data1);',1000);
+	
 }
