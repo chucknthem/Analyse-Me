11c502f88bcfdb0e14c4f5e61b5a26f5d3612c31
Charles Ma <chuckjma@gmail.com>
2010-05-02 14:28:53 +1000

    code cleanup

--- js/db.js
+++ js/db.js
@@ -4,15 +4,22 @@ var DB = (function() {
 		'connectDB':function() {
 			this.db = window.openDatabase("analyseMe", "0.1", "Analyse Me Data", 1024*1024);
 			if(!this.db) {                                                      
-				localStorage['err'] = "database connection failed";
-			} else {
-				localStorage['err'] = null;
-			}
+				console.error("database connection failed")
+			} 
 		},
+		/*
+		 * Supported parameters: startDate
+		 */
 		'fetchPerHour':function(params, callback) {
 			if(!this.db) return;
 			this.db.transaction(function(tx) {
 				var sql = "SELECT host, date, hour, SUM(count) as count FROM analyseMe GROUP BY host, date, hour";
+				if (params) {
+					sql += " WHERE ";
+					if(typeof(params['startDate'] != 'undefined')) {
+						sql += " hour >= '" + params['startDate'] + "'";
+					}
+				}
 				tx.executeSql(sql, [],
 					function(tx, result) {
 						var data = [];
@@ -23,7 +30,7 @@ var DB = (function() {
 						callback(data);
 					},
 					function(tx, error) {
-						localStorage['err'] = "select error:" + error;
+						console.error("select error:" + error.message);
 					}
 				);
 			});//end transaction
@@ -42,7 +49,7 @@ var DB = (function() {
 						callback(data);
 					},
 					function(tx, error) {
-						localStorage['err'] = "select error:" + error;
+						console.error("select error:" + error.message);
 					}
 				);
 			});//end transaction
@@ -64,7 +71,7 @@ var DB = (function() {
 					}
 					callback(data);
 				}, function(tx, error) {
-					localStorage['err'] = "select error:" + error;
+					console.error("select error:" + error.message);
 				}
 				);
 			});
@@ -78,10 +85,10 @@ var DB = (function() {
 					function(tx) {
 						tx.executeSql("SELECT COUNT(*) FROM analyseMe", [], null, 
 							function(tx, error) {
-								localStorage['err'] = "failed to select:" + error;
+								console.error("failed to select:" + error.message);
 								tx.executeSql("CREATE TABLE analyseMe(id INTEGER PRIMARY KEY AUTOINCREMENT, host TEXT NOT NULL, date TEXT NOT NULL, hour TINYINT NOT NULL, count INTEGER DEFAULT '0')", [], null, 
 									function(tx, error) {
-										localStorage['err'] = "failed to create SQL table:" + JSON.stringify(error);
+										console.error("failed to create SQL table:" + JSON.stringify(error));
 									}
 								);
 							}
@@ -92,12 +99,11 @@ var DB = (function() {
 			'insertRow':function(rowObj) {
 				if(!this.db) return;
 					this.db.transaction(function(tx) {
-						tx.executeSql("INSERT INTO analyseMe (host, date, hour, count) VALUES ('" + 
-							rowObj['host'] + "','" + rowObj['date'] + "','" + rowObj['hour'] + "', '1')", 
-							[], 
+						tx.executeSql("INSERT INTO analyseMe (host, date, hour, count) VALUES (?,?,?,?)", 
+							[rowObj['host'], rowObj['date'], rowObj['hour'], 1], 
 							null,
 						function(tx, error) {
-							localStorage['err'] = "failed to load SQL table:" + JSON.stringify(error);
+							console.error("failed to load SQL table:" + JSON.stringify(error));
 						});
 					});
 				},
@@ -106,31 +112,42 @@ var DB = (function() {
 			 * increment the count in a given row. If the row doesn't exit, insert it
 			 */
 		'updateRow': function(rowObj) {
-				if(!this.db) return;
-					this.db.transaction(function(tx) {
-						var sqlStr = "SELECT count FROM analyseMe WHERE host='" + 
-							rowObj['host'] + "' AND date='" + rowObj['date'] + "' AND hour='" + rowObj['hour'] + "'";
-						tx.executeSql(sqlStr, 
-							[], 
-							function(tx, result) { //select succeed
-								if(result.rows.length == 0) {
-									f.insertRow(rowObj);
-								} else {
-									var count = result.rows.item(0)['count'] + 1;
-									var sqlStr = "UPDATE analyseMe SET count='" + count + "' WHERE host='" + 
-										rowObj['host'] + "' AND date='" + rowObj['date'] + "' AND hour='" + rowObj['hour'] + "'";
-										tx.executeSql(sqlStr, [], null,
-										function(tx, error) {
-											localStorage['err'] = "failed to update SQL table:" + JSON.stringify(error);
-										}); //end execute update
-								} //close else
-							},
-							function(tx, error) { //select error
-								localStorage['err'] = "failed to execute select SQL table:" + JSON.stringify(error);
-							}
-						); //end execute select
-					}); //end db transaction
-				}
+			if(!this.db) return;
+			this.db.transaction(function(tx) {
+				var sqlStr = "SELECT count FROM analyseMe WHERE host=? AND date=? AND hour=?";
+				tx.executeSql(sqlStr, [rowObj['host'], rowObj['date'], rowObj['hour']],
+					function(tx, result) { //select succeed
+						if(result.rows.length == 0) {
+							f.insertRow(rowObj);
+						} else {
+							var count = result.rows.item(0)['count'] + 1;
+							var sqlStr = "UPDATE analyseMe SET count=? WHERE host=? AND date=? AND hour=?"
+								tx.executeSql(sqlStr,
+								  [count, rowObj['host'], rowObj['date'], rowObj['hour']],
+									null,
+								function(tx, error) {
+									console.error("failed to update SQL table:" + JSON.stringify(error));
+								}); //end execute update
+						} //close else
+					},
+					function(tx, error) { //select error
+						console.error("failed to execute select SQL table:" + JSON.stringify(error));
+					}
+				); //end execute select
+			}); //end db transaction
+		},//end updateRow
+
+		'deleteAll':function() {
+			if(!this.db) return;
+			this.db.transaction(function(tx) {
+				var sqlStr = "DELETE FROM analyseMe";
+				tx.executeSql(sqlStr, [], null,
+							function(tx, error) {
+								console.error("delete Failed");
+							}); //end execute update
+			});
+		} //end deleteAll
+
 	};
 	f.connectDB();
 	return f;
