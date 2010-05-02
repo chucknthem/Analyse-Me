1000c977ebaa57b5441ecf9641d11c69f5ab586a
Andrew R <andrewreds@gmail.com>
2010-05-02 13:10:26 +1000

    fixed height on graphs

--- popup.js
+++ popup.js
@@ -3,10 +3,24 @@ bar = {};
 var graphit = function (names,values) {
 
 	document.getElementById('overallUsage').width = document.getElementById('overallUsage').width;
+
+	max = names[0].length;
+	for (i=1;i<names.length;i++) {
+		if (names[i].length > max) {
+			max = names[i].length;
+		}
+	}
+	
+	var size = max*2.5;
+	
+	document.getElementById('overallUsage').height = 300 + 2*size;
+	document.getElementById('overallUsage').width = 400 + size*4;
+	document.getElementById('overallUsage').style.cssText = "margin-top:-"+size+"px; margin-bottom:-"+size+"px; margin-right:-"+size*2+"px;";
+	document.body.height=310;
 	
 	bar = new RGraph.HBar('overallUsage', values);
 	bar.Set('chart.labels', names);
-	bar.Set('chart.gutter', 60);
+	bar.Set('chart.gutter', size);
 	bar.Set('chart.background.barcolor1', 'rgba(255,255,255,1)');
 	bar.Set('chart.background.barcolor2', 'rgba(255,255,255,1)');
 	bar.Set('chart.background.grid', true);
