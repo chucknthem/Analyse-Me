f9043d4c301eeddf4d021a9763f765b69061d7ca
Charles Ma <chuckjma@gmail.com>
2010-05-15 00:21:30 +1000

    version  number in zip file

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 working_dir=`pwd`
 cd ..
 
-zip -r $working_dir/analyzeMe.zip $working_dir -x \*docs\* -x \*examples* -x \*build.sh -x \*.git\* -x \*RGraph/images\* -x \*analyzeMe.zip\* \*Makefile\*
+version=`grep 'version' $working_dir/manifest.json | tr '"' '\n' |grep '[0-9]'`
+
+zip -r $working_dir/analyzeMe-$version.zip $working_dir -x \*docs\* -x \*examples* -x \*build.sh -x \*.git\* -x \*RGraph/images\* -x \*analyzeMe\*.zip\* -x \*Makefile\* -x \*excanvas.original.js
